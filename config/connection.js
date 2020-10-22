fd92ed376831d20fa50f08f79fb29ca05ff97371
Tasha <xtasherx@gmail.com>
2020-10-21 23:41:25 -0500

    Corrected ENV handling for DB connection

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,10 @@
 const mysql = require("mysql");
 
 
-
-
+if (process.env.JAWSDB_URL){
+  console.log('Detected envvar JAWSDB_URL, using that...');
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
   var connection = mysql.createConnection({
     host: process.env.MYSQL_HOST || "localhost",
     port: process.env.MYSQL_PORT || 3306,
@@ -10,6 +12,9 @@ const mysql = require("mysql");
     user: process.env.MYSQL_USER,
     database: process.env.MYSQL_DB
   });
+};
+
+
 
 
 
